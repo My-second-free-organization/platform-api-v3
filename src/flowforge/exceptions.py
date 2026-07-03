4bdc86061f0bf67375c9dea24dc5d956fc4a57c2
Dari0o <mise0210@gmail.com>
2026-07-03 23:54:41 +0200

    style: format exception classes for improved readability

--- src/flowforge/exceptions.py
+++ src/flowforge/exceptions.py
@@ -1,5 +1,18 @@
-class FlowForgeError(Exception): pass
-class AuthenticationError(FlowForgeError): pass
-class NotFoundError(FlowForgeError): pass
-class RateLimitError(FlowForgeError): pass
-class ValidationError(FlowForgeError): pass
+class FlowForgeError(Exception):
+    pass
+
+
+class AuthenticationError(FlowForgeError):
+    pass
+
+
+class NotFoundError(FlowForgeError):
+    pass
+
+
+class RateLimitError(FlowForgeError):
+    pass
+
+
+class ValidationError(FlowForgeError):
+    pass
